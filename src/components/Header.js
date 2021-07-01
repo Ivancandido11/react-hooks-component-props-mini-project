317c2d1963b12a01a35b9e58280f4e3245c05417
Ivan Candido <ivan.candido@powerhrg.com>
2021-07-01 10:20:58 -0400

    ArticleList and Article Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React from "react"
 
-const Header = ({ name }) => 
-    (
-        <h1> {name} </h1>
-    )
-    export default Header
+const Header = ({ name }) => (
+  <header>
+    <h1> {name} </h1>
+  </header>
+)
+    
+export default Header
